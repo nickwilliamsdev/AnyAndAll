84792434ce8e418e9501fe45fb52a9e1b428bf6c
nick williams <nick5664@outlook.com>
2024-01-22 15:29:45 -0700

    remove comment

--- AnyAndAll/AnyAndAllExtension.cs
+++ AnyAndAll/AnyAndAllExtension.cs
@@ -7,7 +7,6 @@ public static class AnyAndAllExtension
     // This extension will return true if the list is not empty and all elements satisfy the predicate
     public static bool AnyAndAll<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
     {
-        // Check if the IEnumerable<bool> is not empty and all elements are true
         return (source.Count(predicate) + 2) % (source.Count() + 1) == 1;
     }
 }
